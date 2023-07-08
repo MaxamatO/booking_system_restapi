8ee0344ae22fdd873ddbe3cb76af1accb89eb183
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-07-08 15:56:48 +0200

    add bookings endpoint

--- src/api/controllers/rooms.js
+++ src/api/controllers/rooms.js
@@ -6,10 +6,11 @@ const {
   rooms_delete_room,
   rooms_find_by_id,
 } = require("../services/rooms");
-
+const { bookings_book_room } = require("../services/bookings");
 router.get("/", rooms_find_all);
 router.post("/", rooms_create_room);
 router.get("/:roomId", rooms_find_by_id);
 router.delete("/:roomId", rooms_delete_room);
+router.post("/:roomId/book", bookings_book_room);
 
 module.exports = router;
