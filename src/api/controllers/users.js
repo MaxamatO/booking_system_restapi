f7eb2ded1edefc025f4c515861a80fb31149ce61
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-07-09 16:49:42 +0200

    get user by id

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const { users_get_all, users_register } = require("../services/users");
+const {
+  users_get_all,
+  users_register,
+  users_get_by_id,
+} = require("../services/users");
 
 router.get("/", users_get_all);
 router.post("/register", users_register);
+router.get("/:userId", users_get_by_id);
 
 module.exports = router;
