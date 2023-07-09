a82e8282b5389323563f92a0f32dd99b7a5aedbd
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-07-09 16:49:36 +0200

    get booking by id

--- src/api/controllers/bookings.js
+++ src/api/controllers/bookings.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
-const { bookings_get_all } = require("../services/bookings");
+const {
+  bookings_get_all,
+  bookings_get_by_id,
+} = require("../services/bookings");
 
 router.get("/", bookings_get_all);
+router.get("/:bookingId", bookings_get_by_id);
 
 module.exports = router;
