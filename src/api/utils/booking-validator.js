e353add79fd1fead567f1a3ad200e3dd2b8cae9a
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-07-09 16:37:58 +0200

    validate booking request

---
+++ src/api/utils/booking-validator.js
@@ -0,0 +1,20 @@
+const { Model } = require("mongoose");
+
+/**
+ *
+ * @param {Date} startDate
+ * @param {Date} endDate
+ */
+module.exports.are_dates_valid = (startDate, endDate) => {
+  return new Date(endDate).getTime() >= new Date(startDate).getTime();
+};
+
+/**
+ *
+ * @param {Model} booking
+ */
+module.exports.is_room_booked = async (booking, roomId) => {
+  const isRoomBooked = await booking.find({ roomId: roomId }).exec();
+  console.log(isRoomBooked.length);
+  return isRoomBooked.length >= 1;
+};
