f9f8e3078be09b340a00808ea1c8936943ab19a7
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-07-11 15:18:41 +0200

    add find bookings for room

--- src/api/controllers/rooms.js
+++ src/api/controllers/rooms.js
@@ -5,6 +5,7 @@ const {
   rooms_create_room,
   rooms_delete_room,
   rooms_find_by_id,
+  rooms_find_by_id_with_bookings,
 } = require("../services/rooms");
 const { bookings_book_room } = require("../services/bookings");
 const { checkAuth } = require("../auth/check-auth");
@@ -12,6 +13,7 @@ const { checkAuth } = require("../auth/check-auth");
 router.get("/", rooms_find_all);
 router.post("/", checkAuth, rooms_create_room);
 router.get("/:roomId", rooms_find_by_id);
+router.get("/:roomId/bookings", checkAuth, rooms_find_by_id_with_bookings);
 router.delete("/:roomId", checkAuth, rooms_delete_room);
 router.post("/:roomId/book", checkAuth, bookings_book_room);
 
