91b06b0e448f1203db00caf36bdaf9851bf2016d
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-07-11 15:18:31 +0200

    delete booking

--- src/api/controllers/bookings.js
+++ src/api/controllers/bookings.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const {
   bookings_get_all,
   bookings_get_by_id,
+  bookings_delete_booking,
 } = require("../services/bookings");
 const { checkAuth } = require("../auth/check-auth");
 
 router.get("/", checkAuth, bookings_get_all);
 router.get("/:bookingId", checkAuth, bookings_get_by_id);
+router.delete("/:bookingId", checkAuth, bookings_delete_booking);
 
 module.exports = router;
